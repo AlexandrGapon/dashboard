71990209261ac66922796e2e3d7de537b25f4d46
AlexandrGapon <gapon769@gmail.com>
2021-10-08 12:07:21 +0300

    untyped app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Switch, Route, HashRouter } from 'react-router-dom'
+import { useTests } from './hooks/useTests'
 import DashboardPage from './pages/DashboardPage'
 import FinalizePage from './pages/FinalizePage'
 import ResultsPage from './pages/ResultsPage'
@@ -9,15 +10,48 @@ import { FETCH_TESTS } from './store/reducers/testsActions'
 function App() {
   const dispatch = useDispatch()
 
+  const [query, setQuery] = useState('')
+  const tests = useSelector(state => state.tests.tests)
+  const sites = useSelector(state => state.sites.sites)
+
+  const statusMap = {
+    ONLINE: 1,
+    PAUSED: 2,
+    STOPPED: 3,
+    DRAFT: 4
+  }
+
+
+  let modTests = tests.map(test => {
+    return {
+      id: test.id,
+      name: test.name,
+      type: test.type,
+      status: test.status,
+      siteId: test.siteId,
+      statusSort: statusMap[test.status],
+      site: sites[test.siteId]
+    }
+  })
+
+  const { sortedAndSearchedTests, requestSort, sortConfig, setSortConfig } = useTests({ tests: modTests, query })
+
   useEffect(() => {
     dispatch({ type: FETCH_TESTS })
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path='/'>
-          <DashboardPage />
+          <DashboardPage
+            sortedAndSearchedTests={sortedAndSearchedTests}
+            requestSort={requestSort}
+            sortConfig={sortConfig}
+            setSortConfig={setSortConfig}
+            query={query}
+            setQuery={setQuery}
+          />
         </Route>
         <Route exact path='/results/:id'>
           <ResultsPage />
@@ -26,7 +60,7 @@ function App() {
           <FinalizePage />
         </Route>
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,36 +1,9 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react'
 import SearchString from '../components/SearchString'
-import { useTests } from '../hooks/useTests'
 import '../styles/DashboardPage.css'
 import Dashboard from '../components/Dashboard'
 
-const DashboardPage = () => {
-    const [query, setQuery] = useState('')
-    const tests = useSelector(state => state.tests.tests)
-    const sites = useSelector(state => state.sites.sites)
-
-    const statusMap = {
-        ONLINE: 1,
-        PAUSED: 2,
-        STOPPED: 3,
-        DRAFT: 4
-    }
-
-
-    let modTests = tests.map(test => {
-        return {
-            id: test.id,
-            name: test.name,
-            type: test.type,
-            status: test.status,
-            siteId: test.siteId,
-            statusSort: statusMap[test.status],
-            site: sites[test.siteId]
-        }
-    })
-
-    const { sortedAndSearchedTests, requestSort, sortConfig, setSortConfig } = useTests({ tests: modTests, query })
+const DashboardPage = ({ sortedAndSearchedTests, requestSort, sortConfig, setSortConfig, query, setQuery }) => {
 
     const getClassNameFor = (name) => {
         if (!sortConfig) {
